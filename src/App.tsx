b2dbc089191fe418852a9e15e46e3b59128f2f90
Pastel <pastel.dev.gt@gmail.com>
2022-06-22 21:34:07 -0600

    Set default color scheme to blue

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import {
+  ChakraProvider,
+  extendTheme,
+  withDefaultColorScheme,
+} from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import { HomePage, PostForm, NotFoundPage } from './pages/index'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create-post" element={<PostForm />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-  </ChakraProvider>
-)
+export const App = (): JSX.Element => {
+  const theme = extendTheme(withDefaultColorScheme({ colorScheme: 'blue' }))
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create-post" element={<PostForm />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  )
+}
