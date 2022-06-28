8a6201fceb36f7b69013a9ae37f01ce492472cf0
Pastel <pastel.dev.gt@gmail.com>
2022-06-28 12:42:11 -0600

    Wrap application in PostsContainer to get postsContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import {
 } from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import { HomePage, PostForm, NotFoundPage } from './pages/index'
+import PostsContainer from './context/postsContext'
+import { HomePage, PostForm, NotFoundPage } from './pages'
 
 export const App = (): JSX.Element => {
-  const theme = extendTheme(withDefaultColorScheme({ colorScheme: 'blue' }))
+  const theme = extendTheme(withDefaultColorScheme({ colorScheme: 'twitter' }))
 
   return (
     <ChakraProvider theme={theme}>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create-post" element={<PostForm />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <PostsContainer>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create-post" element={<PostForm />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </PostsContainer>
       </Router>
     </ChakraProvider>
   )
